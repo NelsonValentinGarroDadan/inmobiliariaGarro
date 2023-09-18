e053ce80ed8c984e5fb48b437df5eb2a5a3fb93e
NelsonValentinGarroDadan <nelsonvgarro@sanluis.edu.ar>
2023-09-18 14:51:29 -0300

    cambios propietarios
    controller,view

--- Controllers/PropietariosController.cs
+++ Controllers/PropietariosController.cs
@@ -15,12 +15,12 @@ using Org.BouncyCastle.Crypto.Tls;
 
 namespace inmobiliaria.Controllers
 {
-    public class InquilinosController : Controller
+    public class PropietariosController : Controller
     {
         private readonly IConfiguration configuration;
 		private readonly IWebHostEnvironment environment;
 
-		public InquilinosController(IConfiguration configuration, IWebHostEnvironment environment)
+		public PropietariosController(IConfiguration configuration, IWebHostEnvironment environment)
 		{
 			this.configuration = configuration;
 			this.environment = environment;
@@ -30,23 +30,23 @@ namespace inmobiliaria.Controllers
         [Authorize]
         public ActionResult Index()
         {
-           
-            ViewBag.Id = TempData["Id"];
+             
+             ViewBag.Id = TempData["Id"];
             if(TempData.ContainsKey("Mensaje"))
             {
                 ViewBag.Mensaje = TempData["Mensaje"];
 
             }
-            var IR = new InquilinosRepositorio();
-            var I = IR.ObtenerTodos();
-            return View(I);
+            var PR = new PropietariosRepositorio();
+            var P = PR.ObtenerTodos();
+            return View(P);
         }
         [Authorize]
         public ActionResult Details(int id)
         {
-            var IR = new InquilinosRepositorio();
-            var I = IR.ObtenerXId(id);
-            return View(I);
+            var PR = new PropietariosRepositorio();
+            var P = PR.ObtenerXId(id);
+            return View(P);
         }
         [Authorize]
         public ActionResult Create()
@@ -58,75 +58,75 @@ namespace inmobiliaria.Controllers
 
             }
             var UR = new UsuariosRepositorio();
-            var I = new Inquilinos();
+            var P = new Propietarios();
             ViewBag.Usuarios = UR.ObtenerTodos();
-            return View(I);
+            return View(P);
         }
 
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Inquilinos i)
-        { //Validaciones de Entrada 
+        public ActionResult Create(Propietarios p)
+        { 
             //Validaciones de Entrada 
-            if(i.Id==0 && String.IsNullOrEmpty(i.UsuarioId.DNI) && String.IsNullOrEmpty(i.UsuarioId.Nombre) && String.IsNullOrEmpty(i.UsuarioId.Apellido) && i.UsuarioId.Telefono == 0 && String.IsNullOrEmpty(i.UsuarioId.Mail))
+            if(p.Id==0 && String.IsNullOrEmpty(p.UsuarioId.DNI) && String.IsNullOrEmpty(p.UsuarioId.Nombre) && String.IsNullOrEmpty(p.UsuarioId.Apellido) && p.UsuarioId.Telefono == 0 && String.IsNullOrEmpty(p.UsuarioId.Mail))
             {
                 TempData["Mensaje"]="Debes Elegir un Usuario o crear uno";
                 return RedirectToAction(nameof(Create)); 
             }
-            if(i.Id != 0 && (!String.IsNullOrEmpty(i.UsuarioId.DNI)|| !String.IsNullOrEmpty(i.UsuarioId.Nombre) || !String.IsNullOrEmpty(i.UsuarioId.Apellido) || i.UsuarioId.Telefono != 0 || !String.IsNullOrEmpty(i.UsuarioId.Mail)))
+            if(p.Id != 0 && (!String.IsNullOrEmpty(p.UsuarioId.DNI)|| !String.IsNullOrEmpty(p.UsuarioId.Nombre) || !String.IsNullOrEmpty(p.UsuarioId.Apellido) || p.UsuarioId.Telefono != 0 || !String.IsNullOrEmpty(p.UsuarioId.Mail)))
             {
                TempData["Mensaje"]="Debes Elegir un Usuario o crear uno, no puede hacer ambos";
                 return RedirectToAction(nameof(Create)); 
             }
-            if(i.Id == 0 )
+            if(p.Id == 0 )
             {
-                if (String.IsNullOrEmpty(i.UsuarioId.DNI))
+                if (String.IsNullOrEmpty(p.UsuarioId.DNI))
                 {
                     TempData["Mensaje"]="El campo de DNI es obligatorio";
                     ModelState.AddModelError("UsuarioId.DNI", "El campo DNI es obligatorio.");
                     return RedirectToAction(nameof(Create));
-                }else if(!Expresiones.ValidarDNI(i.UsuarioId.DNI.ToString())){
+                }else if(!Expresiones.ValidarDNI(p.UsuarioId.DNI.ToString())){
                     TempData["Mensaje"]="El DNI no es valido, se permiten solo 7 o 8 caracteres";
                     ModelState.AddModelError("UsuarioId.DNI", "El DNI no es valido, se permiten solo 7 o 8 caracteres.");
                     return RedirectToAction(nameof(Create));
                 }
-                if (string.IsNullOrEmpty(i.UsuarioId.Nombre))
+                if (string.IsNullOrEmpty(p.UsuarioId.Nombre))
                 {
                     TempData["Mensaje"]="El campo de Nombre es obligatorio";
                     ModelState.AddModelError("UsuarioId.Nombre", "El campo Nombre es obligatorio.");
                     return RedirectToAction(nameof(Create));
-                }else if(!Expresiones.ValidarNombre(i.UsuarioId.Nombre)){
+                }else if(!Expresiones.ValidarNombre(p.UsuarioId.Nombre)){
                     TempData["Mensaje"]="El Nombre es valido";
                     ModelState.AddModelError("UsuarioId.Nombre", "El Nombre es valido");
                     return RedirectToAction(nameof(Create));
                 }
-                if (string.IsNullOrEmpty(i.UsuarioId.Apellido))
+                if (string.IsNullOrEmpty(p.UsuarioId.Apellido))
                 {
                     TempData["Mensaje"]="El campo de Apellido es obligatorio";
                     ModelState.AddModelError("UsuarioId.Apellido", "El campo Apellido es obligatorio.");
                     return RedirectToAction(nameof(Create));
-                } else if(!Expresiones.ValidarApellido(i.UsuarioId.Apellido)){
+                } else if(!Expresiones.ValidarApellido(p.UsuarioId.Apellido)){
                     TempData["Mensaje"]="El Apellido no es valido";
                     ModelState.AddModelError("UsuarioId.Apellido", "El Apellido no es valido.");
                     return RedirectToAction(nameof(Create));
                 }
-                if (i.UsuarioId.Telefono < 0)
+                if (p.UsuarioId.Telefono < 0)
                 {
                     TempData["Mensaje"]="El campo de Telefono es obligatorio";
                     ModelState.AddModelError("UsuarioId.Telefono", "El campo Telefono es obligatorio.");
                     return RedirectToAction(nameof(Create));
-                }else if(!Expresiones.ValidarTelefono(i.UsuarioId.Telefono.ToString())){
+                }else if(!Expresiones.ValidarTelefono(p.UsuarioId.Telefono.ToString())){
                     TempData["Mensaje"]="El Telefono no es valido";
                     ModelState.AddModelError("UsuarioId.Telefono", "El Telefono no es valido.");
                     return RedirectToAction(nameof(Create));
                 }
-                if (string.IsNullOrEmpty(i.UsuarioId.Mail))
+                if (string.IsNullOrEmpty(p.UsuarioId.Mail))
                 {
                     TempData["Mensaje"]="El campo de Mail es obligatorio";
                     ModelState.AddModelError("UsuarioId.Mail", "El campo Mail es obligatorio.");
                     return RedirectToAction(nameof(Create));
-                }else if(!Expresiones.ValidarMail(i.UsuarioId.Mail)){
+                }else if(!Expresiones.ValidarMail(p.UsuarioId.Mail)){
                     TempData["Mensaje"]="El Mail es invalido, deberia estar en formato 'example@example.com'";
                     ModelState.AddModelError("UsuarioId.Mail", "El Mail es invalido");
                     return RedirectToAction(nameof(Create));
@@ -135,17 +135,17 @@ namespace inmobiliaria.Controllers
             try
             {
                 var UR = new UsuariosRepositorio();
-                var IR = new InquilinosRepositorio();
-                i.UsuarioId.Id = i.Id;
-                if(UR.Existe(i.UsuarioId)){
-                    IR.Alta(i);
-                    TempData["Mensaje"] = "Se creo con exito el inquilino y se asocio al usuario con id: "+i.Id;
+                var PR = new PropietariosRepositorio();
+                p.UsuarioId.Id = p.Id;
+                if(UR.Existe(p.UsuarioId)){
+                    PR.Alta(p);
+                    TempData["Mensaje"] = "Se creo con exito el inquilino y se asocio al usuario con id: "+p.Id;
                 }else{
-                    i.UsuarioId.Nombre = char.ToUpper(i.UsuarioId.Nombre[0]) + i.UsuarioId.Nombre.Substring(1).ToLower();
-                    i.UsuarioId.Apellido = char.ToUpper(i.UsuarioId.Apellido[0]) + i.UsuarioId.Apellido.Substring(1).ToLower();
-                    i.Id = UR.Alta(i.UsuarioId);
-                    IR.Alta(i);
-                    TempData["Mensaje"] = "Se creo con exito el inquilino y el usuario nuevo con id: "+i.Id;
+                    p.UsuarioId.Nombre = char.ToUpper(p.UsuarioId.Nombre[0]) + p.UsuarioId.Nombre.Substring(1).ToLower();
+                    p.UsuarioId.Apellido = char.ToUpper(p.UsuarioId.Apellido[0]) + p.UsuarioId.Apellido.Substring(1).ToLower();
+                    p.Id = UR.Alta(p.UsuarioId);
+                    PR.Alta(p);
+                    TempData["Mensaje"] = "Se creo con exito el inquilino y el usuario nuevo con id: "+p.Id;
                     
                 }
                 
@@ -157,12 +157,12 @@ namespace inmobiliaria.Controllers
                 Console.WriteLine(E.Message);
                 return RedirectToAction(nameof(Create));
             }
-        }
+        } 
          [Authorize]
         // GET: Usuarios/Edit/5
         public ActionResult Edit(int id)
         { 
-            var IR = new InquilinosRepositorio();
+            var PR = new PropietariosRepositorio();
             
             ViewBag.Id = TempData["Id"];
             if(TempData.ContainsKey("Mensaje"))
@@ -170,51 +170,51 @@ namespace inmobiliaria.Controllers
                 ViewBag.Mensaje = TempData["Mensaje"];
 
             }
-            return View(IR.ObtenerXId(id));
+            return View(PR.ObtenerXId(id));
         }
         [Authorize]
         // POST: Usuarios/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, Inquilinos i)
+        public ActionResult Edit(int id, Propietarios p)
         {
              //Validaciones de Entrada 
-            if (string.IsNullOrEmpty(i.UsuarioId.DNI))
+            if (string.IsNullOrEmpty(p.UsuarioId.DNI))
             {
                 TempData["Mensaje"]="El campo de DNI es obligatorio";
                 ModelState.AddModelError("UsuarioId.DNI", "El campo DNI es obligatorio.");
                 return RedirectToAction(nameof(Create));
-            }else if( !Expresiones.ValidarDNI(i.UsuarioId.DNI.ToString())){
+            }else if( !Expresiones.ValidarDNI(p.UsuarioId.DNI.ToString())){
                 TempData["Mensaje"]="El DNI no es valido, se permiten solo 7 o 8 caracteres";
                 ModelState.AddModelError("UsuarioId.DNI", "El DNI no es valido, se permiten solo 7 o 8 caracteres.");
                 return RedirectToAction(nameof(Create));
             }
-            if (string.IsNullOrEmpty(i.UsuarioId.Nombre))
+            if (string.IsNullOrEmpty(p.UsuarioId.Nombre))
             {
                 TempData["Mensaje"]="El campo de Nombre es obligatorio";
                 ModelState.AddModelError("UsuarioId.Nombre", "El campo Nombre es obligatorio.");
                 return RedirectToAction(nameof(Create));
-            }else if( !Expresiones.ValidarNombre(i.UsuarioId.Nombre)){
+            }else if( !Expresiones.ValidarNombre(p.UsuarioId.Nombre)){
                 TempData["Mensaje"]="El Nombre es valido";
                 ModelState.AddModelError("UsuarioId.Nombre", "El Nombre es valido");
                 return RedirectToAction(nameof(Create));
             }
-            if (string.IsNullOrEmpty(i.UsuarioId.Apellido))
+            if (string.IsNullOrEmpty(p.UsuarioId.Apellido))
             {
                 TempData["Mensaje"]="El campo de Apellido es obligatorio";
                 ModelState.AddModelError("UsuarioId.Apellido", "El campo Apellido es obligatorio.");
                 return RedirectToAction(nameof(Create));
-            } else if(!Expresiones.ValidarApellido(i.UsuarioId.Apellido)){
+            } else if(!Expresiones.ValidarApellido(p.UsuarioId.Apellido)){
                 TempData["Mensaje"]="El Apellido no es valido";
                 ModelState.AddModelError("UsuarioId.Apellido", "El Apellido no es valido.");
                 return RedirectToAction(nameof(Create));
             }
-            if (i.UsuarioId.Telefono < 0)
+            if (p.UsuarioId.Telefono < 0)
             {
                 TempData["Mensaje"]="El campo de Telefono es obligatorio";
                 ModelState.AddModelError("UsuarioId.Telefono", "El campo Telefono es obligatorio.");
                 return RedirectToAction(nameof(Create));
-            }else if(!Expresiones.ValidarTelefono(i.UsuarioId.Telefono.ToString())){
+            }else if(!Expresiones.ValidarTelefono(p.UsuarioId.Telefono.ToString())){
                 TempData["Mensaje"]="El Telefono no es valido";
                 ModelState.AddModelError("UsuarioId.Telefono", "El Telefono no es valido.");
                 return RedirectToAction(nameof(Create));
@@ -222,19 +222,18 @@ namespace inmobiliaria.Controllers
             try
             {   
                 var UR = new UsuariosRepositorio();
-                var IR = new InquilinosRepositorio();
                 //Primera Mayuscula Nombre y Apelldo
-                i.UsuarioId.Nombre = char.ToUpper(i.UsuarioId.Nombre[0]) + i.UsuarioId.Nombre.Substring(1).ToLower();
-                i.UsuarioId.Apellido = char.ToUpper(i.UsuarioId.Apellido[0]) + i.UsuarioId.Apellido.Substring(1).ToLower();
-                i.UsuarioId.Id = i.Id;
-                var bol =UR.Modificacion(id,i.UsuarioId);
+                p.UsuarioId.Nombre = char.ToUpper(p.UsuarioId.Nombre[0]) + p.UsuarioId.Nombre.Substring(1).ToLower();
+                p.UsuarioId.Apellido = char.ToUpper(p.UsuarioId.Apellido[0]) + p.UsuarioId.Apellido.Substring(1).ToLower();
+                p.UsuarioId.Id = p.Id;
+                var bol =UR.Modificacion(id,p.UsuarioId);
                 if(bol)
                 {
-                  TempData["Mensaje"]="Se modifico con exito la entidad id:"+i.Id;
+                  TempData["Mensaje"]="Se modifico con exito la entidad id:"+p.Id;
                 return RedirectToAction(nameof(Index));  
                 }else
                 {
-                    throw new Exception("No se pudo modificar con exito la entidad con id:"+i.Id);
+                    throw new Exception("No se pudo modificar con exito la entidad con id:"+p.Id);
                 }
                 
             }
@@ -259,16 +258,16 @@ namespace inmobiliaria.Controllers
                 ViewBag.Mensaje = TempData["Mensaje"];
 
             }
-            var IR = new InquilinosRepositorio();
-            var I = IR.ObtenerXId(id);    
-            return View(I);
+            var PR = new PropietariosRepositorio();
+            var P = PR.ObtenerXId(id);    
+            return View(P);
         }
 
         // POST Empleados/Delete/5
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, Inquilinos i)
+        public ActionResult Delete(int id, Propietarios e)
         {
             try
             {
@@ -277,8 +276,8 @@ namespace inmobiliaria.Controllers
                         TempData["Mensaje"] = "No tienes permiso de realizar esta accion";
                         return RedirectToAction(nameof(Index), "Home"); 
             }
-                var IR = new InquilinosRepositorio();
-                var bol = IR.Baja(i);
+                var PR = new PropietariosRepositorio();
+                var bol = PR.Baja(e);
                 if(bol)
                 {
                     TempData["Mensaje"] = "Se elimino con exito la entidad";
@@ -295,5 +294,6 @@ namespace inmobiliaria.Controllers
                 return RedirectToAction(nameof(Delete));
             }
         }
+    
    }    
 }
