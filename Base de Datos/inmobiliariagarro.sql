44d44904a8728fefef4e515db1b24dbe98187b99
NelsonValentinGarroDadan <nelsonvgarro@sanluis.edu.ar>
2023-09-20 14:47:19 -0300

    bd

--- Base de Datos/inmobiliariagarro.sql
+++ Base de Datos/inmobiliariagarro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-09-2023 a las 00:42:49
+-- Tiempo de generación: 20-09-2023 a las 19:45:06
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -62,7 +62,13 @@ CREATE TABLE `contratos` (
 
 INSERT INTO `contratos` (`Id`, `FechaInicio`, `FechaFin`, `InquilinoId`, `InmuebleId`, `Importe`) VALUES
 (27, '2023-09-02', '2023-09-19', 58, 17, '50001'),
-(28, '2023-09-23', '2023-09-30', 58, 17, '50000');
+(28, '2023-09-23', '2023-09-19', 58, 17, '50000'),
+(30, '2023-10-03', '2023-12-09', 58, 17, '98000'),
+(31, '2023-09-19', '2024-01-05', 63, 18, '50000'),
+(32, '2023-09-19', '2023-09-29', 63, 19, '34000'),
+(34, '2023-09-19', '2023-09-30', 67, 17, '50000'),
+(35, '2023-09-02', '2023-09-20', 67, 20, '1000'),
+(37, '2023-09-02', '2023-09-30', 68, 22, '1000');
 
 -- --------------------------------------------------------
 
@@ -76,6 +82,13 @@ CREATE TABLE `empleados` (
   `Clave` varchar(250) NOT NULL,
   `Avatar` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`Id`, `UsuarioId`, `Clave`, `Avatar`) VALUES
+(61, 61, 'cH2faJaCcOHv3PL0SgvKNPU5nakY3ld92Vbaqpzw9Q==', NULL);
 
 -- --------------------------------------------------------
 
@@ -101,7 +114,11 @@ CREATE TABLE `inmuebles` (
 --
 
 INSERT INTO `inmuebles` (`Id`, `CA`, `Direccion`, `Precio`, `PropietarioId`, `TipoUsoId`, `Latitud`, `Longitud`, `TipoEstadoId`, `TipoInmuebleId`) VALUES
-(17, 3, 'Modulo 13', '70000', 57, 12, '-35.463', '-180.0', 101, 1);
+(17, 3, 'Modulo 13', '70000', 57, 12, '-35.463', '-180.0', 101, 1),
+(18, 6, 'carlos gardel 654', '32000000', 64, 12, '-35.463', '-180.0', 101, 1),
+(19, 2, 'Pancha Herndez 45', '450000000', 67, 12, '-35.463', '-180.0', 102, 2),
+(20, 2, 'MOdulo 11', '70000', 68, 11, '-35.463', '-180.0', 101, 4),
+(22, 3, 'MOdulo 23', '70000', 36, 12, '-35.463', '-180.0', 101, 2);
 
 -- --------------------------------------------------------
 
@@ -119,7 +136,12 @@ CREATE TABLE `inquilinos` (
 --
 
 INSERT INTO `inquilinos` (`Id`, `UsuarioId`) VALUES
-(58, 58);
+(58, 58),
+(62, 62),
+(63, 63),
+(66, 66),
+(67, 67),
+(68, 68);
 
 -- --------------------------------------------------------
 
@@ -139,7 +161,11 @@ CREATE TABLE `pagos` (
 --
 
 INSERT INTO `pagos` (`Id`, `ContratoId`, `Fecha`, `Importe`) VALUES
-(5, 27, '0001-01-25', '1000');
+(5, 27, '0001-01-25', '1000'),
+(6, 27, '0001-01-01', '45000'),
+(7, 28, '0001-01-01', '45000'),
+(8, 28, '0001-01-01', '230000'),
+(9, 32, '2023-09-20', '3000');
 
 -- --------------------------------------------------------
 
@@ -157,7 +183,12 @@ CREATE TABLE `propietarios` (
 --
 
 INSERT INTO `propietarios` (`Id`, `UsuarioId`) VALUES
-(57, 57);
+(36, 36),
+(57, 57),
+(64, 64),
+(65, 65),
+(67, 67),
+(68, 68);
 
 -- --------------------------------------------------------
 
@@ -239,8 +270,18 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`Id`, `DNI`, `Nombre`, `Apellido`, `Telefono`, `Mail`) VALUES
 (36, '44643100', 'Nelson', 'Garro', 2664910392, 'nelsonvgarro@sanluis.edu.ar'),
-(57, '12345678', 'Felicito', 'Quiroga', 12345678, 'example@example.com'),
-(58, '12345678', 'Anastasio', 'Segundo', 123456789, 'example@example.com');
+(57, '12345678', 'Amargado', 'Quiroga', 12345678, 'example@example.com'),
+(58, '12345678', 'Burlao', 'Segundo', 123456789, 'example@example.com'),
+(59, '24983199', 'Quiricocho', 'Dadan', 2664729009, 'nicovaleauri@gmail.com'),
+(60, '24983199', 'Mariela', 'Dadan', 2664729009, 'nicovaleauri@gmail.com'),
+(61, '12345679', 'Aurora', 'Garro', 123456789, 'example@gmail.com'),
+(62, '42914999', 'Nicolas', 'Dadan', 234567890, 'example@gmail.com'),
+(63, '21876398', 'Leandro', 'Zuares', 98765432, 'example@gmail.com'),
+(64, '32654786', 'Juan', 'Juan', 234523456, 'example@example.com'),
+(65, '65234789', 'Gustavo', 'Sosa', 453298765, 'example@example.com'),
+(66, '23456876', 'Ana', 'Videla', 243658798, 'example@gmail.com'),
+(67, '23456789', 'Gustavo', 'Quiroga', 2435676767, 'example@example.com'),
+(68, '12345678', 'Fernando', 'Erwin', 123456789, 'example@example.com');
 
 --
 -- Índices para tablas volcadas
@@ -331,43 +372,43 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `administradores`
 --
 ALTER TABLE `administradores`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de la tabla `contratos`
 --
 ALTER TABLE `contratos`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT de la tabla `empleados`
 --
 ALTER TABLE `empleados`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT de la tabla `inmuebles`
 --
 ALTER TABLE `inmuebles`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `inquilinos`
 --
 ALTER TABLE `inquilinos`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT de la tabla `pagos`
 --
 ALTER TABLE `pagos`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `propietarios`
 --
 ALTER TABLE `propietarios`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT de la tabla `tiposestados`
@@ -391,7 +432,7 @@ ALTER TABLE `tiposusos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- Restricciones para tablas volcadas
